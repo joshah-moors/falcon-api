7cc2503a032110b1a4205ee60e75bd3025147506
Joshah <joshah.moors@utexas.edu>
2020-03-14 11:25:47 -0500

    Added POST route and unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,28 @@
-#! /usr/bin/env python3
+import io
+
+# Python 3
+from unittest.mock import call, MagicMock, mock_open
 
 import falcon
+from falcon import testing
 import msgpack
 import pytest
-from falcon import testing
-from unittest.mock import mock_open, call
 
-from falconapi.app import api
+import falconapi.app
+import falconapi.images
 
 
 @pytest.fixture
-def client():
+def mock_store():
+    return MagicMock()
+
+
+@pytest.fixture
+def client(mock_store):
+    api = falconapi.app.create_app(mock_store)
     return testing.TestClient(api)
 
-# Pytest will inject the the object returned by the fixture client function
+
 def test_list_images(client):
     doc = {
         'images': [
@@ -29,23 +38,27 @@ def test_list_images(client):
     assert result_doc == doc
     assert response.status == falcon.HTTP_OK
 
-# Monkeypatch - fixture used to install mocks
-def test_posted_image_gets_saved(client, monkeypatch):
-    mock_file_open = mock_open()
-    monkeypatch.setattr('io.open', mock_file_open)
 
-    fake_uuid = '123e4567-e89b-12d3-a456-426655440000'
-    monkeypatch.setattr('uuid.uuid4', lambda: fakeuuid)
+# With clever composition of fixtures, we can observe what happens with
+# the mock injected into the image resource.
+def test_post_image(client, mock_store):
+    file_name = 'fake-image-name.xyz'
 
-    # When service receives an image through POST
-    fake_image_bytes = b'fake-images-bytes'
+    # We need to know what ImageStore method will be used
+    mock_store.save.return_value = file_name
+    image_content_type = 'image/xyz'
+
     response = client.simulate_post(
         '/images',
-        body=fake_image_bytes,
-        headers={'content-type': 'image/png'}
-        )
+        body=b'some-fake-bytes',
+        headers={'content-type': image_content_type}
+    )
 
-# return 201, save the file, return the resource location
-assert response.status == falcon.HTTP_CREATED
-assert call().write(fake_image_bytes) in mock_file_open.mock_calls
-assert resposne.headers['location'] == f'/images/{fake_uuid}.png'
+    assert response.status == falcon.HTTP_CREATED
+    assert response.headers['location'] == '/images/{}'.format(file_name)
+    saver_call = mock_store.save.call_args
+
+    # saver_call is a unittest.mock.call tuple. It's first element is a
+    # tuple of positional arguments supplied when calling the mock.
+    #assert isinstance(saver_call[0][0], falcon.request_helpers.BoundedStream)
+    assert saver_call[0][1] == image_content_type

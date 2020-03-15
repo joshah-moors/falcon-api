719d039757d5b3f76cb21615989d46292179725c
Joshah <joshah.moors@utexas.edu>
2020-03-15 18:16:31 -0500

    rearranged

--- jwtapi/app.py
+++ jwtapi/app.py
@@ -14,6 +14,24 @@ from .app_functions import *
 # This is held in the environment somewhere
 APP_SECRET = 'Yz#SZ4The0AJU^jC'
 
+class Authenticate:
+    def on_post(self, req, resp):
+        # Parse user/pass out of the body
+        user = req.media['user']
+        pw = req.media['password']
+        #print(f'RECEIVED: User: {user} -- Pass: {pw}')
+        #
+        #   ~ VERIFY USER LOGIC GOES HERE ~
+        #
+        jwt = jwt_auth.get_auth_token({'username': user})
+        resp_dict = {
+            'accessToken': jwt,
+            'refreshToken': '',
+            'refreshAge': '',
+        }
+        resp.body = json.dumps(resp_dict)
+        resp.status = falcon.HTTP_200
+
 # JWT Backend & Middleware
 user_loader = lambda token_content: token_content['user']
 jwt_auth = JWTAuthBackend(user_loader, APP_SECRET)
--- jwtapi/app_functions.py
+++ jwtapi/app_functions.py
@@ -3,27 +3,6 @@
 import json
 
 import falcon
-from falcon_auth import FalconAuthMiddleware, JWTAuthBackend
-
-
-class Authenticate:
-    def on_post(self, req, resp):
-        # Parse user/pass out of the body
-        user = req.media['user']
-        pw = req.media['password']
-        #print(f'RECEIVED: User: {user} -- Pass: {pw}')
-        #
-        #   ~ VERIFY USER LOGIC GOES HERE ~
-        #
-        jwt = jwt_auth.get_auth_token({'username': user})
-        resp_dict = {
-            'accessToken': jwt,
-            'refreshToken': '',
-            'refreshAge': '',
-        }
-        resp.body = json.dumps(resp_dict)
-        resp.status = falcon.HTTP_200
-
 
 class PublicInfo:
     def on_get(self, req, resp):

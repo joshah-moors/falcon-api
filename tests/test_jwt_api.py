a48ecfac81555b309accaf92f3d35675d08355c7
Joshah <joshah.moors@utexas.edu>
2020-03-22 20:22:21 -0500

    Unit test for public route

--- tests/test_jwt_api.py
+++ tests/test_jwt_api.py
@@ -1,7 +1,20 @@
 #! /usr/bin/env python3
 
+import falcon
 import pytest
 
+import jwtapi.app
 
-def test_the_testcase():
-    pass
+
+@pytest.fixture
+def client():
+    api = jwtapi.app.create_app()
+    return falcon.testing.TestClient(api)
+
+
+def test_the_testcase(client):
+    ''' Test the open route '''
+    response = client.simulate_get('/media/api/v1/public')
+    assert response.status == falcon.HTTP_OK
+    assert response.json['status'] == 'success'
+    assert response.json['data'] == 'it\'s all good'

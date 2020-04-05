e72ad9fdc614e2d2b0f80fa805199c5ea39f9231
Joshah <joshah.moors@utexas.edu>
2020-04-05 17:04:19 -0500

    Renamed api routes

--- jwtapi/app.py
+++ jwtapi/app.py
@@ -14,10 +14,10 @@ import jwtapi.app_resources as app_resources
 from jwtapi.app_db import Session, SQLAlchemySessionManager
 
 
-open_routes = ['/auth/api/v1/login',
-               '/auth/api/v1/refresh',
-               '/auth/api/v1/user-mgmt',
-               '/media/api/v1/public', ]
+open_routes = ['/api/v1/auth/login',
+               '/api/v1/auth/refresh',
+               '/api/v1/auth/user-mgmt',
+               '/api/v1/media/public', ]
 
 auth_middleware = FalconAuthMiddleware(app_auth.jwt_auth, exempt_routes=open_routes)
 db_middleware = SQLAlchemySessionManager(Session)
@@ -26,12 +26,12 @@ cors_middleware = app_auth.CORSComponent()
 
 def create_app():
     api = falcon.API(middleware=[auth_middleware, db_middleware, cors_middleware])
-    api.add_route('/auth/api/v1/login', app_auth.Login())
-    api.add_route('/auth/api/v1/refresh', app_auth.RefreshToken())
-    api.add_route('/auth/api/v1/invalidate', app_auth.InvalidateToken())
-    api.add_route('/auth/api/v1/user-mgmt', app_auth.UserMgmt())
-    api.add_route('/media/api/v1/public', app_resources.PublicInfo())
-    api.add_route('/media/api/v1/private', app_resources.PrivateInfo())
+    api.add_route('/api/v1/auth/login', app_auth.Login())
+    api.add_route('/api/v1/auth/refresh', app_auth.RefreshToken())
+    api.add_route('/api/v1/auth/invalidate', app_auth.InvalidateToken())
+    api.add_route('/api/v1/auth/user-mgmt', app_auth.UserMgmt())
+    api.add_route('/api/v1/media/public', app_resources.PublicInfo())
+    api.add_route('/api/v1/media/private', app_resources.PrivateInfo())
     return api
 
 
